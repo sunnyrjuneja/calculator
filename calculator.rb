71c9926feb3851de0ae3400b4ec55bc15d2bd23f
Sunny Juneja <jr.sunny@gmail.com>
2012-10-20 17:45:26 -0700

    completed

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,19 @@
+require 'debugger'
+
 class Calculator
-  def initialize(str)
+  attr_reader :str
+  def initialize
     @operations = /([\+\-\* \^\\])/
-    str.gsub! /\s/, ""
-    str = str.split ""
-    find_expression(str)
   end
 
-  def find_expression(str)
+  def eval str
+    str.gsub! /\s/, ""
+    str = str.split ""
+    @str = str
+    (find_expression @str)[0].to_i
+  end
+
+  def find_expression str
     leftparan = 0
     rightparan = 0
     index = nil
@@ -18,18 +25,20 @@ class Calculator
         rightparan += 1       
       end
       if leftparan > 0 && rightparan == leftparan
-        eval_expression str[index..i]
-        leftparan = 0
-        rightparan = 0
+        str[index..i] = find_expression str[index + 1...i]
+        find_expression str
       end
     end
-    return str
+    return eval_expression str
   end
 
   def eval_expression arr
-    print arr
-    find_expression arr[1...arr.length - 1]
+    while arr.length > 1
+      arr[1] = "**" if arr[1] == "^"
+      value = arr[0].to_i.send arr[1].to_sym, arr[2].to_i
+      arr = arr.drop(3)
+      arr.insert 0, value.to_s
+    end
+    return arr
   end
 end
-
-c = Calculator.new "(1 + 2) + (4 + 3))"
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -15,4 +15,9 @@ describe Calculator do
     it { @c.eval("4 * 3").should be 12 }
     it { @c.eval("3 ^ 2").should be 9 }
   end
+
+  describe "can deal with parans" do
+    it { @c.eval("(1 + 2) + (3 + 4)").should be 10 }
+    it { @c.eval("(1 + 2) + (3 + (4 + 5))").should be 15 }
+  end
 end
